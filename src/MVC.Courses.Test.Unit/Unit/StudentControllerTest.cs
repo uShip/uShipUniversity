670eee35e41c8e61d86d2fefa731ef4d611aa19d
Ben Willkommen <bwillkommen@uship.com>
2017-12-21 09:46:05 -0600

    added StudentController class and unit test for GetAgent

---
+++ src/MVC.Courses.Test.Unit/Unit/StudentControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using MVC.Courses.Web.Controllers;
+using NUnit.Framework;
+
+namespace MVC.Courses.Test.Unit.Unit
+{
+    [TestFixture(Category = "UnitTest")]
+    public class StudentControllerTest
+    {
+        [Test]
+        public void GetAgent_should_return_browser_name()
+        {
+            //arrange
+            var userAgent = "IE6 :(";
+
+            var request = new Mock<HttpRequestBase>();
+            request.SetupGet(r => r.Browser.Browser).Returns(userAgent);
+
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(c => c.Request).Returns(request.Object);
+
+            var studentController = new StudentController();
+            studentController.ControllerContext =
+                new ControllerContext(context.Object, new RouteData(), studentController);
+
+            //act
+            var result = studentController.GetAgent();
+
+            //assert
+            var jsonResult = result as JsonResult;
+            Assert.AreEqual(userAgent, jsonResult.Data);
+
+        }
+    }
+}
---
+++ src/MVC.Courses.Web/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Courses.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult GetAgent()
+        {
+            return Json(Request.Browser.Browser, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
