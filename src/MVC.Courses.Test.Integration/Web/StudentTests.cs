aa089a2ad0d7f6970e37d81cd0e5b34b4a4b230f
Ben Willkommen <bwillkommen@uship.com>
2017-12-21 10:04:16 -0600

    added StudentController.GetAgent e2e test

---
+++ src/MVC.Courses.Test.Integration/Web/StudentTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Should;
+
+namespace MVC.Courses.Test.Integration.Web
+{
+    [TestFixture(Category = "Selenium")]
+    public class StudentTests : SeleniumTest
+    {
+        public StudentTests() : base("MVC.Courses.WEB")
+        {
+        }
+
+        [TestCase]
+        public void GetAgent_should_return_user_agent()
+        {
+            var userAgent = "Chrome";
+
+            WebDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Student/GetAgent"));
+            WebDriver.PageSource.ShouldContain(userAgent);
+        }
+    }
+}
