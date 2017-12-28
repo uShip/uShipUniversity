5f4d622c31f63be6aec72120c40ffa25773ff6b2
TC Schiller <tc@tcschiller.com>
2017-12-28 15:22:38 -0600

    Add Student Controller

---
+++ src/MVC.Courses.Test.Integration/Web/StudentTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace MVC.Courses.Test.Integration.Web
+{
+    [TestFixture(Category = "Selenium")]
+    public class StudentTests : SeleniumTest
+    {
+        public StudentTests() : base("MVC.Courses.WEB") { }
+
+        [TestCase]
+        public void GiveMeJson_should_return_json()
+        {
+            WebDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Student/GetAgent"));
+            var source = WebDriver.PageSource;
+            source.ShouldNotBeEmpty();
+        }
+    }
+}
---
+++ src/MVC.Courses.Web/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MVC.Courses.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public JsonResult GetAgent()
+        {
+            var fact = $"You are using {Request.Browser.Browser}.. probably.";
+            var opinion = Request.Browser.Browser == "InternetExplorer"
+                ? "You really should get a better browser."
+                : "That is just splendid.";
+
+            var responseData = JsonConvert.SerializeObject(new 
+            {
+                result = $"{fact} {opinion}"
+            });
+
+            return new JsonResult
+            {
+                ContentEncoding = Encoding.UTF8,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = responseData
+            };
+        }
+    }
+}
