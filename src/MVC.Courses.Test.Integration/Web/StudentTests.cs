9fa00031ddcd781eca428c0641986df04ece3111
Robert Myers <rmyers@uship.com>
2017-12-29 11:01:13 -0600

    Add Student/GetAgent path

---
+++ src/MVC.Courses.Test.Integration/Web/StudentTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace MVC.Courses.Test.Integration.Web
+{
+    [TestFixture]
+    public class StudentTests : SeleniumTest
+    {
+        public StudentTests() : base("MVC.Courses.Web")
+        {
+
+        }
+
+        [Test]
+        public void GetAgent_should_return_browser_type()
+        {
+            WebDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Student/GetAgent"));
+            var source = WebDriver.PageSource;
+            source.ShouldContain("Chrome");
+        }
+    }
+}
---
+++ src/MVC.Courses.Web/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace MVC.Courses.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetAgent()
+        {
+            var agent = Request.Browser.Browser;            
+            var rc = new JsonResult
+            {
+                Data = new { agent },
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                ContentEncoding = Encoding.UTF8,
+                ContentType = "application/json"
+            };
+            return rc;
+        }
+    }
+}
