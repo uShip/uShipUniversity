8ab3052b928e79fd4e965e47b9881ee80583731f
Robert Myers <developer@robandjen.com>
2018-01-03 21:45:26 -0600

    Use visual studio iis (#3)
    * Enable using running Visual Studio instance for tests
    
    * Testing Improvements
    
     * Only start IIS once per fixture
     * Use Visual Studio instance if already running
     * Get rid of all resharper warnings in the base class.

--- src/MVC.Courses.Test.Integration/IisServerTestBase.cs
+++ src/MVC.Courses.Test.Integration/IisServerTestBase.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace MVC.Courses.Test.Integration
 {   
     public abstract class IisServerTestBase
     {
-        const int IisPort = 2042;
+        private const ushort IisPort = 2042;
+        private const ushort VisualStudioPort = 50544;
+
         private readonly string _applicationName;
         private Process _iisProcess;
 
@@ -16,55 +20,89 @@ namespace MVC.Courses.Test.Integration
             _applicationName = applicationName;
         }
 
+        [OneTimeSetUp]
+        public Task FixtureInitialze()
+        {
+            // Start IISExpress
+            return StartIis();
+        }
+
         [SetUp]
         public void TestInitialize()
         {
-            // Start IISExpress
-            StartIIS();
-
             Initialize();
         }
 
-        [TearDown]
-        public void TestCleanup()
+        [OneTimeTearDown]
+        public void FixtureCleanup()
         {
-            Cleanup();
             // Ensure IISExpress is stopped
-            if (_iisProcess.HasExited == false)
+            if (_iisProcess?.HasExited == false)
             {
                 _iisProcess.Kill();
             }
         }
 
+        [TearDown]
+        public void TestCleanup()
+        {
+            Cleanup();           
+        }
+
         public abstract void Initialize();
         public abstract void Cleanup();
 
-        private void StartIIS()
+        private static async Task<bool> IsVsIisRunning()
         {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    using (var response = await client.GetAsync($"http://localhost:{VisualStudioPort}/Home/Status"))
+                    {
+                        return response.IsSuccessStatusCode;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+        private async Task StartIis()
+        {
+            _iisProcess = null;
+            if (await IsVsIisRunning())
+            {
+                return;
+            }
+
             var applicationPath = GetApplicationPath(_applicationName);
             var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
 
-            _iisProcess = new Process();
-            _iisProcess.StartInfo.FileName = Path.Combine(programFiles,"IIS Express","iisexpress.exe");
-            _iisProcess.StartInfo.Arguments = $"/path:\"{applicationPath}\" /port:{IisPort}";
+            _iisProcess = new Process
+            {
+                StartInfo =
+                {
+                    FileName = Path.Combine(programFiles, "IIS Express", "iisexpress.exe"),
+                    Arguments = $"/path:\"{applicationPath}\" /port:{IisPort}"
+                }
+            };
             _iisProcess.Start();
         }
 
         protected virtual string GetApplicationPath(string applicationName)
         {
             var baseDir = AppDomain.CurrentDomain.BaseDirectory;            
-            var solutionFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..",".."));
+            var solutionFolder = Path.GetFullPath(Path.Combine(baseDir, "..", "..",".."));
             
             return Path.Combine(solutionFolder, applicationName);
         }
 
+        private ushort CurrentPort => _iisProcess != null ? IisPort : VisualStudioPort;
         public string GetAbsoluteUrl(string relativeUrl)
         {
-            if (!relativeUrl.StartsWith("/"))
-            {
-                relativeUrl = "/" + relativeUrl;
-            }
-            return $"http://localhost:{IisPort}/{relativeUrl}";
+            return $"http://localhost:{CurrentPort}{(relativeUrl.StartsWith("/") ? "" : "/")}{relativeUrl}";
         }
     }
 }
