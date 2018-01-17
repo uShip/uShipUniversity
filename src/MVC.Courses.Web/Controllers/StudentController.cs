263002e585f5c0333c40ad2e04272501f5c1d794
Ben Yen <byen@uship.com>
2018-01-17 17:08:33 -0600

    Added StudentController

---
+++ src/MVC.Courses.Web/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MVC.Courses.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult GetAgent()
+        {
+            return new JsonResult
+            {
+                ContentEncoding = Encoding.ASCII,
+                Data = JsonConvert.SerializeObject(Request.Browser, Formatting.None),
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+}
