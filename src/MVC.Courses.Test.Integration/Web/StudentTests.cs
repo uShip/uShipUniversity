d2f7f1937a933a6a2bbed8c364f52557d2c50313
Bryan Drenner <bdrenner@uship.com>
2018-01-23 13:26:03 -0600

    Back end course 1 homework: /Student/GetAgent, first draft

---
+++ src/MVC.Courses.Test.Integration/Web/StudentTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Should;
+
+namespace MVC.Courses.Test.Integration.Web
+{
+    [TestFixture(Category = "Selenium")]
+    public class StudentTests : SeleniumTest
+    {
+        public StudentTests() : base("MVC.Courses.WEB") { }
+
+        [TestCase]
+        public void Student_GetAgent_should_return_Chrome()
+        {
+            WebDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Student/GetAgent"));
+            var output = WebDriver.FindElement(By.TagName("pre")).Text;
+            output.ShouldEqual("\"Chrome\"");
+        }
+    }
+}
---
+++ src/MVC.Courses.Web/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Courses.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult GetAgent()
+        {
+            return Json(Request.Browser.Browser, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
