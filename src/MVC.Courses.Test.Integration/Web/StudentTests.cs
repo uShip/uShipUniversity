e881f78f5aea937a0e051fa2c74833a420c6b432
Ben Yen <byen@uship.com>
2018-01-29 17:01:30 -0600

    Added test

---
+++ src/MVC.Courses.Test.Integration/Web/StudentTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Should;
+
+namespace MVC.Courses.Test.Integration.Web
+{
+    [TestFixture(Category = "Selenium")]
+    public class StudentTests : SeleniumTest
+    {
+        public StudentTests() : base("MVC.Courses.WEB") { }
+
+        [TestCase]
+        public void GetAgent_should_return_Chrome()
+        {
+            WebDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Student/GetAgent"));
+            WebDriver.PageSource.ShouldContain("Chrome");
+        }
+    }
+}
