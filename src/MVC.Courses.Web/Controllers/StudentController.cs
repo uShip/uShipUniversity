bbc82f5bd69d9cffd7c778d91f88737e421fe4e7
James Gillin <jgillin@uship.com>
2018-01-29 16:02:00 -0600

    New student controller with the Agent endpoint

---
+++ src/MVC.Courses.Web/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MVC.Courses.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public JsonResult Agent()
+        {
+            object responseData = JsonConvert.SerializeObject(Request.Browser.Browser, Formatting.None);
+            return new JsonResult
+            {
+                ContentEncoding = Encoding.ASCII,
+                Data = responseData,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+}
