6725cdc29af2d1100693ed198b906a3f20099074
Zachariah Grummons <zgrummons@uship.com>
2018-02-08 14:53:03 -0600

    HW 1.2 done

---
+++ src/MVC.Courses.Test.Integration/Web/StudentTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace MVC.Courses.Test.Integration.Web
+{
+    [TestFixture(Category = "Selenium")]
+    public class StudentTests : SeleniumTest
+    {
+        public StudentTests() : base("MVC.Courses.WEB") { }
+
+        [TestCase]
+        public void student_getAgent_should_return_chrome()
+        {
+            WebDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Student/GetAgent"));
+            WebDriver.PageSource.ShouldContain("\\\"Browser\\\":\\\"Chrome\\\"");
+        }
+
+    }
+}
